0bded6aa3ac2b34681c0435a271fa6986c6a0a31
Zsobrák Krisztián <kryssz90@gmail.com>
2022-01-11 16:57:06 +0100

    Fix missing space

--- src/components/forms/start.jsx
+++ src/components/forms/start.jsx
@@ -37,7 +37,7 @@ const Start = ({ setStarted }) => (
         <ExternalLink
           href="https://www.linkedin.com/in/krisztian-zsobrak/"
           text="Zsobrák Krisztián"
-        />
+        />{' '}
         és Pál Bettina.
       </Text>
 
